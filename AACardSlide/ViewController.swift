a4bbf4ff047779634c8351dbb4d639fa9a01a7b1
Amin <amin030296@gmail.com>
2020-09-26 16:13:17 -0400

    Initial Commit

---
+++ AACardSlide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AACardSlide
+//
+//  Created by Aminjoni Abdullozoda on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
